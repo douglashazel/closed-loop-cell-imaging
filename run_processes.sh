cdb080689fb87e4406d54dedb5b433d515314b1f
Douglas Hazel <douglas.hazel@tufts.edu>
2025-09-18 15:27:22 -0400

    future proofing

--- run_processes.sh
+++ run_processes.sh
@@ -4,9 +4,9 @@ set -euo pipefail
 # -----------------------------
 # User parameters
 # -----------------------------
-IMAGE_DIR="frames"
-MASK_DIR="masks"
-SAVE_PATH="analysis"
+IMAGE_DIR="TEST/frames"
+MASK_DIR="TEST/masks"
+SAVE_PATH="TEST/analysis"
 FLOW_THRESHOLD=0.4       # default=0.4; higher = stricter flow consistency, fewer masks
 CELLPROB_THRESHOLD=0.0   # default=0.0; higher = fewer cells accepted, lower = more cells
 NITER=200                # default=200; higher = slower but can improve accuracy
