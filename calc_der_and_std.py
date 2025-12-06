d8c73a20856fc96787630fc0a196dd0cf7c57936
Douglas Hazel <douglas.hazel@tufts.edu>
2025-12-06 15:39:13 -0500

    std plot incorporated into calc_der plot

--- calc_der_and_std.py
+++ calc_der_and_std.py
@@ -4,7 +4,29 @@ import pandas as pd
 from tqdm import tqdm
 import matplotlib.pyplot as plt
 
-experiments = [item for item in os.listdir() if os.path.isdir(item) and item not in ["gifs", "archive"]]
+def plot_luminosity_std(traj_csv, save_path, tag):
+    df = pd.read_csv(traj_csv, index_col=0)
+    df_transposed = df.T
+    std_per_frame = df_transposed.std(axis=1)
+
+    frames = [int(str(x).lstrip("f")) for x in std_per_frame.index]
+    std_values = std_per_frame.values
+
+    plt.figure(dpi=300)
+    plt.plot(frames, std_values, 'o-', color='darkred', alpha=0.7, markersize=3)
+    plt.xlabel("Frame")
+    plt.ylabel("Standard Deviation of Luminosity (Across all cells)")
+    plt.title("Luminosity Variability Over Time")
+    plt.tight_layout()
+
+    plot_dir = os.path.join(save_path, "plots")
+    os.makedirs(plot_dir, exist_ok=True)
+
+    plot_name = f'std_average_luminosity{tag}.png'
+    plt.savefig(os.path.join(plot_dir, plot_name), dpi=300)
+    plt.close()
+
+experiments = [item for item in os.listdir() if os.path.isdir(item) and item not in ["gifs", "archive"]][:1]
 
 for exp in tqdm(experiments, desc='Calculating...'):
     analysis_dir_path = os.path.join(exp, 'analysis')
@@ -35,5 +57,8 @@ for exp in tqdm(experiments, desc='Calculating...'):
 
             plt.savefig(f'{analysis_dir_path}/plots/luminosity_derivative_complete.png')
             plt.close()
+
+            plot_luminosity_std(file_path, analysis_dir_path, "")
+
         except TypeError:
             print(f"Error with {exp}")
