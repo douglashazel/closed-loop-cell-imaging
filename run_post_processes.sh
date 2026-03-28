2453f1988a02e5ea71b0731ef95b304ef7786706
Douglas Hazel <douglas.hazel@tufts.edu>
2026-03-28 18:00:48 -0400

    new figures

--- run_post_processes.sh
+++ run_post_processes.sh
@@ -4,15 +4,16 @@ set -euo pipefail
 # -----------------------------
 # PATHS
 # -----------------------------
-GLOBAL_DIR="EXPERIMENTS/other/NRK_acidpulse_3:17:26_edited"
+GLOBAL_DIR="EXPERIMENTS/other/DOUG_nrk"
 IMAGE_DIR="${GLOBAL_DIR}/frames"
 ANALYSIS_DIR="${GLOBAL_DIR}/analysis"
 
 # -----------------------------
 # ANALYSIS PARAMETERS
 # -----------------------------
-F0_FRAME=2400
-STIM_FRAMES="77,112,148,183,218,322,357,390,424,458" # C2C12 DMSO
+F0_FRAME=1
+# STIM_FRAMES="77,112,148,183,218,322,357,390,424,458" # C2C12 DMSO
+STIM_FRAMES="0,9,18,26,35,45,56,65,74,83,141,152,164,176,189,200,215" # NRK closed-loop March27 w/ initial setpoint
 
 echo "--- Accessing ${GLOBAL_DIR} ---"
 # -----------------------------
